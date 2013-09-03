606a48942e9a1c7bd881bb4de15bbcffd1072296
Joey Tuong <joey@tetrap.us>
2013-09-04 00:20:29 +1000

    Cleaned some closures

--- plugins/base/autorejoin.py
+++ plugins/base/autorejoin.py
@@ -1,7 +1,6 @@
-def initialise(name, bot, stream):
+def __initialise__(name, bot, stream):
 	def autorejoin(line):
 		words = line.split()
 		if words[3].lower() == bot.nick.lower():
 			stream.raw_message("JOIN %s" % words[2])
 	bot.register("kick", autorejoin)
-__initialise__ = initialise
--- plugins/base/botmode.py
+++ plugins/base/botmode.py
@@ -1,6 +1,4 @@
-def initialise(name, bot, stream):
+def __initialise__(name, bot, stream):
 	def set_botmode(line):
 		stream.raw_message("MODE %s +B" % bot.nick)
 	bot.register("376", set_botmode)
-
-__initialise__ = initialise
