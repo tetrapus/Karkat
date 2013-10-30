c1cc3b8e81fb652d9315f1e9265504a9be46052c
Joey Tuong <joey@tetrap.us>
2013-10-31 01:09:50 +1100

    Smartened up url shortening

--- plugins/shorten.py
+++ plugins/shorten.py
@@ -1,14 +1,32 @@
 from util.services import url as URL
 from util.irc import Callback
+import re
 
-cb = Callback()
+def __initialise__(name, server, printer):
+    cb = Callback()
+    cb.initialise(name, server, printer)
 
-@cb.threadsafe
-@cb.command("shorten shortgo bl bitly bit.ly".split(), "(.+)", 
-			usage="12bit.ly⎟ Usage: !shorten <url>",
-			error="05bit.ly⎟ Unable to generate shortlink.")
-def shortgo(message, url):
-    return "12bit.ly⎟ %s" % URL.format(URL.shorten(url))
+    class LinkGrabber(object):
+            
+        def __init__(self):
+            self.links = {}
+            
+        @Callback.background
+        def trigger_linkget(self, line):
+            x = list(line.split(" "))
+            if x[2][0] == "#":
+                x[3] = x[3][1:]
+                self.links.setdefault(server.lower(x[2]), []).extend([i for i in x[3:] if re.match("^(http|https|ftp)\://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(:[a-zA-Z0-9]*)?/?([a-zA-Z0-9\-\._\?\,\'/\\\+&%\$#\=~])*$", i)])
 
-__initialise__ = cb.initialise
-__callbacks__ = {"privmsg": [shortgo]}
+    lg = LinkGrabber()
+    server.register("privmsg", lg.trigger_linkget)
+
+    @cb.threadsafe
+    @cb.command("shorten shortgo bl bitly bit.ly".split(), "(.*)", private="!", public="@.",
+    			usage="12bit.ly⎟ Usage: !shorten <url>",
+    			error="05bit.ly⎟ Unable to generate shortlink.")
+    def shortgo(message, url):
+        if not url: url = lg.links[server.lower(message.context)][-1]
+        return "12%s⎟ %s" % ("bit.ly" * message.text.startswith("@"), URL.format(URL.shorten(url)))
+
+    server.register("privmsg", shortgo)
