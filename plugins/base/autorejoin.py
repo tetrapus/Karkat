f48845b922397027a3c6564a47bedbbeea581447
Joey Tuong <joey@tetrap.us>
2013-12-23 03:25:40 +1100

    Fixed bug

--- plugins/base/autorejoin.py
+++ plugins/base/autorejoin.py
@@ -3,6 +3,6 @@
 def autorejoin(server, line):
     words = line.split()
     if server.eq(words[3], server.nick):
-        server.stream.raw_message("JOIN %s" % words[2])
+        server.sendline("JOIN %s" % words[2])
 
 __callbacks__ = {"kick": [autorejoin]}
