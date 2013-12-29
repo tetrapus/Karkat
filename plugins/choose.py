c7dabe3c2660d2481a18c7822fb3147bb6cca928
Joey Tuong <joey@tetrap.us>
2013-12-30 03:37:05 +1100

    Fixed filth, added choose

--- plugins/choose.py
+++ plugins/choose.py
@@ -4,6 +4,6 @@ from bot.events import command
 
 @command("choose", "(.+)")
 def choose(server, msg, query):
-    return "\x0309│\x03 " + random.choice(query.split(",").strip())
+    return "\x0309│\x03 " + random.choice(query.split(",")).strip()
 
 __callbacks__ = {"privmsg": [choose]}
