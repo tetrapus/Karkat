e2ee361442f53c0c78118ab8bcdd7b4f2199713f
Joey Tuong <joey@tetrap.us>
2013-12-30 16:10:58 +1100

    Changed nickserv auth semantics

--- karkat.py
+++ karkat.py
@@ -11,7 +11,6 @@ Options:
     -d --debug                          Turn on debugging
     -i PASSWORD, --identify=PASSWORD    Identify with the given password
     -s --stdin                          Take password from STDIN
-    -a --auth                           Auth instead of identify
     -r --restart                        Restart on disconnect
     -c NUM, --connections=NUM           Number of output connections [default: 1]
 """
@@ -24,7 +23,7 @@ from collections import deque
 import docopt
 
 from bot.threads import StatefulBot, Printer, Bot
-from util.irc import Callback
+from util.irc import Callback, Message
 import util.text
 
 __version__ = 2.0
@@ -95,13 +94,17 @@ def main():
     if args["--identify"]:
         def authenticate(server, line):
             """ Sends nickserv credentials after the server preamble. """
-            if args["--auth"]:
-                cmd = "nickserv AUTH %s"
-            else:
-                cmd = "nickserv IDENTIFY %s"
-            server.sendline(cmd % args["--identify"])
+            msg = Message(line)
+            if msg.address.nick == "NickServ":
+                if "is a registered nick." in msg.text:
+                    cmd = "nickserv AUTH %s"
+                elif msg.text.startswith("This nickname is registered."):
+                    cmd = "nickserv IDENTIFY %s"
+                else:
+                    return
+                server.sendline(cmd % args["--identify"])
 
-        server.register("001", authenticate)
+        server.register("notice", authenticate)
     if args["--debug"]:
         @Callback.inline
         def log(server, line):
