3b58be195632403600707ca4516848586bf80568
Joey Tuong <joey@tetrap.us>
2014-02-06 04:53:29 +1100

    implemented primitive tell

--- plugins/tell.py
+++ plugins/tell.py
@@ -1,6 +1,10 @@
+import time
 import re
+import random
 
 from bot.events import Callback, command
+from util.text import pretty_date
+from util.irc import Address, Message
 #from util import scheduler
 
 
@@ -22,6 +26,8 @@ seconds_map = {"w": 604800,
                "second": 1}
 
 def parse_time(expr):
+    if not expr:
+        return 0
     tokens = re.split(r"(\d+|an?)", expr)
     tokens = tokens[1:]
     tokens = [i.strip() for i in tokens]
@@ -39,11 +45,44 @@ class Reminder(Callback):
     def __init__(self, server):
         self.waiting = {}
         self.reminders = {}
+        self.server = server
         super().__init__(server)
 
-    @command("remind tell note send", r"^(?:to\s+)?(\S+)\s+(?:in\s+%(time)s\s+)?(?:that\s+)?(.+?)(?:\s+in\s+%(time)s)?(?:\s+via\s+(snapchat|pm|notice|globally))?(?:\s+every\s+%(time)s(?:\s+until\s+(cancelled|active))?)?$" % {"time": time_expression})
+    @command("remind tell note send", r"^(?:to\s+)?(\S+)\s+(?:(?:in|after)\s+%(time)s\s+)?(?:that\s+)?(.+?)(?:\s+(?:in|after)\s+%(time)s)?(?:\s+via\s+(snapchat|pm|notice|channel message|message|private message))?(?:\s+every\s+%(time)s(?:\s+until\s+(cancelled|active))?)?$" % {"time": time_expression})
     def reminder(self, server, msg, user, after, text, after2, method, repeat, cancel):
-        after = after or after2
+        after = parse_time(after or after2)
+        repeat = parse_time(repeat)
+        method = (method or "channel message").lower()
+        if method == "snapchat":
+            return "Snapchat not yet implemented."
+
+        self.reminders.setdefault(server.lower(user), []).append({"sender": msg.address, "message": text, "method": method, "time": time.time()})
         return "user=%(user)s, after=%(after)s, text=%(text)s, method=%(method)s, repeat=%(repeat)s, cancel=%(cancel)s" % locals()
+
+    def privmsg_check(self, server, line) -> "privmsg":
+        msg = Message(line)
+        address = msg.address
+        if server.lower(address.nick) in self.reminders:
+            self.send_messages(address.nick, msg.context)
+
+    def join_check(self, server, line) -> ["join", "nick"]:
+        hostmask, method, context = line.split()
+        address = Address(hostmask)
+        if method.lower() == "join":
+            channel = context[1:]
+        else:
+            channel = random.choice([i for i in server.channels if server.isIn(address.nick, server.channels[i])])
+        if server.lower(address.nick) in self.reminders:
+            self.send_messages(address.nick, channel)        
+
+    def send_messages(self, user, context):
+        for i in self.reminders[self.server.lower(user)]:
+            method = {"pm": (user, "PRIVMSG"),
+                     "private message": (user, "PRIVMSG"),
+                     "message": (context, "PRIVMSG"),
+                     "channel message": (context, "PRIVMSG"),
+                     "notice": (user, "NOTICE")}[i["method"]]
+            self.server.message("03│ ✉ │ %s: %s · from %s · ⌚ %s" % (user, i["message"], i["sender"], pretty_date(time.time() - i["time"])), *method)
+        self.reminders[self.server.lower(user)] = []
 
 __initialise__ = Reminder
