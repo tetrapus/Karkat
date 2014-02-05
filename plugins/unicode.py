30f875c980d7b88e9e322955c1ad11d28de2adf4
Joey Tuong <joey@tetrap.us>
2014-02-05 23:24:40 +1100

    Cleaned unicode database search module

--- plugins/unicode.py
+++ plugins/unicode.py
@@ -1,118 +1,132 @@
+"""
+Unicode character database search module.
+"""
+
 import unicodedata
+import json
 
 from bot.events import command, Callback
 
-database = open("data/UnicodeData.txt").read().split("\n")
+
+# Load unicode database
+database = open("data/Unicode/UnicodeData.txt").read().split("\n")
 database = [i.split(";") for i in database if i]
 database = {int(i[0], 16): i for i in database}
-blocks = open("data/Blocks.txt").read().split("\n")
+# Load character block ranges
+blocks = open("data/Unicode/Blocks.txt").read().split("\n")
 blocks = [i.split(";") for i in blocks if i.strip() and not i.startswith("#")]
-blocks = [([int(x, 16) for x in i[0].split("..")], i[1].strip()) for i in blocks]
-categories = {
-"Cc": "Other, Control",
-"Cf": "Other, Format",
-"Cn": "Other, Not Assigned",
-"Co": "Other, Private Use",
-"Cs": "Other, Surrogate",
-"LC": "Letter, Cased",
-"Ll": "Letter, Lowercase",
-"Lm": "Letter, Modifier",
-"Lo": "Letter, Other",
-"Lt": "Letter, Titlecase",
-"Lu": "Letter, Uppercase",
-"Mc": "Mark, Spacing Combining",
-"Me": "Mark, Enclosing",
-"Mn": "Mark, Nonspacing",
-"Nd": "Number, Decimal Digit",
-"Nl": "Number, Letter",
-"No": "Number, Other",
-"Pc": "Punctuation, Connector",
-"Pd": "Punctuation, Dash",
-"Pe": "Punctuation, Close",
-"Pf": "Punctuation, Final quote",
-"Pi": "Punctuation, Initial quote",
-"Po": "Punctuation, Other",
-"Ps": "Punctuation, Open",
-"Sc": "Symbol, Currency",
-"Sk": "Symbol, Modifier",
-"Sm": "Symbol, Math",
-"So": "Symbol, Other",
-"Zl": "Separator, Line",
-"Zp": "Separator, Paragraph",
-"Zs": "Separator, Space"
-}
+blocks = [([int(x, 16) for x in i[0].split("..")], i[1].strip()) 
+            for i in blocks]
+# Load category codes
+categories = json.load(open("data/Unicode/Categories.json"))
 
+# Callback templates
 template = {Callback.USAGE: "\x0304Unicode│\x03 .unicode (character|query)",
-            KeyError: "\x0304Unicode│\x03 Character not found."}
+            KeyError:       "\x0304Unicode│\x03 Character not found."}
 template_sym = {Callback.USAGE: "\x0304Unicode│\x03 .symbols (query)",
-            KeyError: "\x0304Unicode│\x03 Character not found."}
+                KeyError:       "\x0304Unicode│\x03 Character not found."}
 
 def getdata(char):
+    """
+    Check if character is recognised by the python unicodedata module.
+    """
     try:
-        return ("%X" % ord(char), unicodedata.name(char), unicodedata.category(char))
+        return ("%X" % ord(char), 
+                unicodedata.name(char), 
+                unicodedata.category(char))
     except:
         raise KeyError("Character not found.")
 
 @Callback.threadsafe
 @command("unicode", "(.+)", templates=template)
 def search(server, message, data):
+    """
+    Search for a specific unicode character and show related information.
+    """
     if len(data) == 1:
+        # Single character, search for the specific character
         try:
             results = [database[ord(data)]]
         except KeyError:
             results = [getdata(data)]
     else:
+        # Check other fields
         data = data.upper()
         results = [i for i in database.values() if any(data == x for x in i)]
-        results += [i for i in database.values() if any(data in x for x in i) and not any(data == x for x in i)]
+        results += [i for i in database.values() 
+                      if any(data in x for x in i) and not any(data == x 
+                                                                for x in i)]
         if not results:
+            # If we can't find it, see if unicodedata has it
             results = [getdata(unicodedata.lookup(data))]
 
     nresults, show = (len(results), 4) if message.prefix != "." else (1, 1)
-    for r in results[:show]:
-        code = int(r[0], 16)
-        for b in blocks:
-            if b[0][0] <= code <= b[0][1]:
-                block = b[1]
+
+    for result in results[:show]:
+        code = int(result[0], 16)
+        # Figure out which block it's in
+        for block in blocks:
+            if block[0][0] <= code <= block[0][1]:
+                charblock = block[1]
                 break
         else:
-            block = "Unknown"
+            charblock = "Unknown"
+
         info = {"module": "Unicode" if message.prefix != "." else "",
                 "symbol": repr(chr(code))[1:-1],
-                "code": r[0],
-                "name": r[1],
-                "block": block,
-                "category": categories[r[2]] if r[2] in categories else "Unknown"
+                "code": result[0],
+                "name": result[1],
+                "block": charblock,
+                "category": categories.get(result[2], "Unknown")
                 }
-        yield "\x0310%(module)s│\x03 %(symbol)s \x0310│\x03 U+%(code)s %(name)s \x0310│\x03 %(block)s · %(category)s" % info
+
+        yield "\x0310%(module)s│\x03 %(symbol)s \x0310│\x03"\
+              " U+%(code)s %(name)s \x0310│\x03"\
+              " %(block)s · %(category)s" % info
+ 
+    # If the user requested more than 4 results, inform them of omitted results
     if nresults > 4:
         yield "\x0310Unicode│\x03 %d of %d results shown." % (4, nresults)
 
 @Callback.threadsafe
 @command("symbols", "(.+)")
 def symbols(server, msg, data):
+    """
+    List unicode characters matching a search string.
+    """
     data = data.upper()
+
+    # Search main database
     results = [i for i in database.values() if any(data in x for x in i)]
+    # Search category names
     if data in [i.upper() for i in categories.values()]:
         category = {v.upper(): k for k, v in categories.items()}[data]
         results += [i for i in database.values() if i[2] == category]
+    # Check category symbols
     if data in [i.upper() for i in categories.keys()]:
         results += [i for i in database.values() if i[2].upper() == data]
+    # Search blocks
     for block in [x[0] for x in blocks if data == x[1].upper()]:
-        results += map(lambda x:("%X"%x,), range(*block))
+        results += ["%X" % i for i in range(block[0], block[1])]
 
     results = [repr(chr(int(i[0], 16)))[1:-1] for i in results]
     results.sort(key=lambda x:x.startswith("\\"))
+
+    # Truncate results
     size = -1
     chars = []
     while results and size < 400:
-        c = results.pop(0)
-        chars.append(c)
-        size += len(c.encode("utf-8", errors="replace")) + 1
+        char = results.pop(0)
+        chars.append(char)
+        size += len(char.encode("utf-8", errors="replace")) + 1
+
     if results:
         chars.append("and %d more." % (len(results)))
+
     if not chars:
         chars = ["No results."]
-    return "\x0310%s│\x03 %s" % ("Unicode" if msg.prefix != "." else "", " ".join(chars))
+
+    return "\x0310%s│\x03 %s" % ("Unicode" if msg.prefix != "." else "",
+                                 " ".join(chars))
 
 __callbacks__ = {"privmsg":[search, symbols]}
