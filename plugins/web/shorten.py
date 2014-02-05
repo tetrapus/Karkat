8bb763713d4308ff3f33e27bd96162c0abe6462c
Joey Tuong <joey@tetrap.us>
2014-02-06 00:24:47 +1100

    Cleaned shorten

--- plugins/web/shorten.py
+++ plugins/web/shorten.py
@@ -1,27 +1,57 @@
+"""
+Interface to bit.ly link shortening service.
+"""
+
 from util.services import url as URL
-from util.irc import Callback, command
+from bot.events import Callback, command
 import re
 
+
+urlexpr = re.compile(r"^(http|https|ftp)\://"\
+                     r"[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}"\
+                     r"(:[a-zA-Z0-9]*)?"\
+                     r"/?([a-zA-Z0-9\-\._\?\,\'/\\\+&%\$#\=~])*$")
+
+templates = {Callback.USAGE: "12│ 🔗 │ Usage: !shorten <url>",
+             Callback.ERROR: "05│ 🔗 │ Unable to generate shortlink."}
+
 class LinkGrabber(object):
-        
+    """
+    Scrapes links from channel messages.
+    """
+
     def __init__(self):
         self.links = {}
         
     @Callback.background
-    def trigger_linkget(self, server, line):
-        x = list(line.split(" "))
-        if x[2][0] == "#":
-            x[3] = x[3][1:]
-            self.links.setdefault(server.lower(x[2]), []).extend([i for i in x[3:] if re.match("^(http|https|ftp)\://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(:[a-zA-Z0-9]*)?/?([a-zA-Z0-9\-\._\?\,\'/\\\+&%\$#\=~])*$", i)])
+    def trigger(self, server, line):
+        """
+        Scrape for URLs
+        """
+        words = list(line.split(" "))
+        if words[2][0] == "#":
+            words[3] = words[3][1:]
+            self.links.setdefault(server.lower(words[2]), 
+                                  []).extend([i for i in words[3:] 
+                                                if urlexpr.match(i)])
 
-lg = LinkGrabber()
+    def last(self, channel):
+        """
+        Return the last linked URL in a channel.
+        """
+        return self.links[channel][-1]
+
+grabber = LinkGrabber()
 
 @Callback.threadsafe
-@command("shorten shortgo bl bitly bit.ly".split(), "(.*)", private="!", public="@.",
-			usage="12│ 🔗 │ Usage: !shorten <url>",
-			error="05│ 🔗 │ Unable to generate shortlink.")
+@command("shorten shortgo bl bitly bit.ly", "(.*)", templates=templates)
 def shortgo(server, message, url):
-    if not url: url = lg.links[server.lower(message.context)][-1]
-    return "12%s│ %s" % ("" * message.text.startswith("@"), URL.format(URL.shorten(url)))
+    """
+    Shorten a URL. If no URL is provided, shorten the last posted URL.
+    """
+    if not url: 
+        url = grabber.last(server.lower(message.context))
+    return "12%s│ %s" % ("🔗" * message.text.startswith("@"), 
+                             URL.format(URL.shorten(url)))
 
-__callbacks__ = {"privmsg": [shortgo, lg.trigger_linkget]}
+__callbacks__ = {"privmsg": [shortgo, grabber.trigger]}
