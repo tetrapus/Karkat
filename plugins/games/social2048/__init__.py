cd7830bd1a5b0c94b5163b565cc6a3762eca86b1
Joey Tuong <joey@tetrap.us>
2014-03-25 16:34:51 +1100

    Added a shitload of new games, persistance

--- plugins/games/social2048/__init__.py
+++ plugins/games/social2048/__init__.py
@@ -1,25 +1,34 @@
+import json
+
 from math import log
 
-from .board import Board
+from . import board as boards
 
 from bot.events import Callback, command
 
 class IRC2048(Callback):
-    colors = [15, 14, 7, 4, 5, 8, 9, 3, 11, 10, 12]
+    colors = [14, 15, 7, 4, 5, 8, 9, 3, 11, 10, 12]
     
     def __init__(self, server):
-        self.games = {}
+        self.savefile = server.get_config_dir("2048.json")
+        try:
+            self.games = json.load(open(self.savefile))
+            for chan, game in self.games.items():
+                self.games[chan] = getattr(boards, game["type"]).unserialise(game)
+        except FileNotFoundError:
+            self.games = {}
         super().__init__(server)
 
     def print_board(self, board):
         for y, row in enumerate(board.board):
-            yield ("\x1f" if y != board.size[1]-1 else "") + "│".join(("\x03%.2d%s\x03" % (self.colors[int(log(cell, 2)) - 1], str(cell).center(4))) if cell else "    " for cell in row) + "\x03"
+            yield "│%s%s│" % ("\x1f" if y != board.size[1]-1 else "", "".join(("\x03,%.2d%s\x03" % (self.colors[int(log(cell, 2)) - 1], str(cell).center(5))) if cell is not None else "    " for cell in row))
 
-    @command("2048")
+    @command("2048 1024 512 256 128 64 32 16 8")
     def start(self, server, msg):
         if server.lower(msg.context) not in self.games:
-            self.games[server.lower(msg.context)] = Board()
+            self.games[server.lower(msg.context)] = Board(goal=int(msg.command))
         board = self.games[server.lower(msg.context)]
+        self.savestate()
         yield from self.print_board(board)
 
     @command("up down left right u d l r")
@@ -38,7 +47,11 @@ class IRC2048(Callback):
 ┃  ┃ ┃┗━┓┣━ ┣┻┓
 ┗━━┗━┛┗━┛┗━━╹ ╹"""
             del self.games[server.lower(msg.context)]
-
+        self.savestate()
         yield from self.print_board(board)
+
+    def savestate(self):
+        with open(self.savefile, "w") as f:
+            json.dump({k: v.serialise() for k, v in self.games.items()}, f)
 
 __initialise__ = IRC2048
--- plugins/games/social2048/board.py
+++ plugins/games/social2048/board.py
@@ -4,20 +4,34 @@ import random
 
 class Board(object):
 
-
-    def __init__(self, size=(4, 4)):
-        self.board = [[0 for i in range(size[0])] for j in range(size[1])]
+    def __init__(self, size=(4, 4), goal=2048, tiles=None, score=0):
         self.size = size
         self.score = 0
-        self.spawn_tile()
-        self.spawn_tile()
+        self.goal = goal
+        if tiles is None:
+            self.board = [[None for i in range(size[0])] for j in range(size[1])]
+            self.spawn_tile()
+            self.spawn_tile()
+        else:
+            self.board = tiles
+
+    @classmethod
+    def unserialise(cls, js):
+        return cls(size=tuple(js["size"]), goal=js["goal"], score=js["score"], tiles=js["tiles"])
+
+    def serialise(self):
+        return {"type": self.__class__.__name__,
+                "size": self.size,
+                "goal": self.goal,
+                "score": self.score,
+                "tiles": self.board}      
 
     def spawn_tile(self):
         # Get empty tiles
         empty = []
         for i in range(self.size[0]):
             for j in range(self.size[1]):
-                if not self.board[j][i]:
+                if self.board[j][i] is None:
                     empty.append((i, j))
         spawn = random.choice(empty)
         self.board[spawn[1]][spawn[0]] = self.random_tile()
@@ -46,7 +60,7 @@ class Board(object):
             return self
 
     def won(self):
-        return any(any(i >= 2048 for i in rows) for rows in self.board)
+        return any(any(i is not None and i >= self.goal for i in rows) for rows in self.board)
 
     def is_endgame(self):
         # Easy test: Check if any move can be made
@@ -59,21 +73,21 @@ class Board(object):
 
     @staticmethod
     def merge(x1, x2):
-        if x1 == x2 and x1:
+        if x1 == x2 and x1 is not None:
             return x1 + x2
 
     @classmethod
     def reduce(cls, vector):
         # Remove 0s
         length = len(vector)
-        v = [i for i in vector if i]
+        v = [i for i in vector if i is not None]
         for i, n in enumerate(v[:-1]):
             merged = cls.merge(v[i], v[i+1])
-            if merged:
-                v[i], v[i+1] = merged, 0
+            if merged is not None:
+                v[i], v[i+1] = merged, None
 
-        v = [i for i in v if i]
-        v += [0] * (length - len(v))
+        v = [i for i in v if i is not None]
+        v += [None] * (length - len(v))
         return v
 
     def __str__(self):
@@ -81,3 +95,117 @@ class Board(object):
 
     def __repr__(self):
         return "\n".join(" | ".join(str(i) for i in j) for j in self.board)
+
+class FibBoard(Board):
+    fibs = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]
+
+    def __init__(goal=377, **kwargs):
+        kwargs.update({"goal":goal})
+        super().__init__(**kwargs)
+
+    @staticmethod
+    def random_tile():
+        return 1
+
+    @staticmethod
+    def merge(*x):
+        if all(x) and sum(x) in FibBoard.fibs:
+            return sum(x)
+
+class ZeroBoard(Board):
+    @staticmethod
+    def random_tile():
+        return random.choice([2, 2, 2, 2, 2, 2, 2, 2, 4, 0])
+
+class DeterministicBoard(Board):
+    def spawn_tile(self):
+        # Get empty tiles
+        tile = None
+        for y, row in enumerate(self.board):
+            for x, cell in enumerate(row):
+                if cell is None:
+                    tile = (x, y)
+                    break
+            if tile is not None:
+                break
+        self.board[tile[1]][tile[0]] = 2
+        return tile
+
+class Easy2048(Board):
+
+    def __init__(self, size=(4, 4), goal=2048):
+        self.board = [[goal/2 for i in range(size[0])] for j in range(size[1])]
+        self.size = size
+        self.score = 0
+        self.goal = goal
+
+class Board3D(Board):
+
+    def __init__(self, size=(3, 3, 3), goal=2048, tiles=None, score=0):
+        self.size = size
+        self.score = 0
+        self.goal = goal
+        if tiles is None:
+            self.board = [[[None for i in range(size[0])] for j in range(size[1])] for k in range(size[2])]
+            self.spawn_tile()
+            self.spawn_tile()
+        else:
+            self.board = tiles
+
+    def spawn_tile(self):
+        # Get empty tiles
+        empty = []
+        for i in range(self.size[0]):
+            for j in range(self.size[1]):
+                for k in range(self.size[2]):
+                    if self.board[k][j][i] is None:
+                        empty.append((i, j, k))
+        spawn = random.choice(empty)
+        self.board[spawn[2]][spawn[1]][spawn[0]] = self.random_tile()
+        return spawn
+
+    def left(self):
+        return [[self.reduce(i) for i in plane] for plane in self.board]
+
+    def right(self):
+        return [[self.reduce(i[::-1])[::-1] for i in plane] for plane in self.board]
+
+    def up(self):
+        return [[list(x) for x in zip(*[self.reduce(i) for i in zip(*plane)])] for plane in self.board]
+
+    def down(self):
+        return [[list(x) for x in zip(*[self.reduce(i[::-1])[::-1] for i in zip(*plane)])] for plane in self.board]
+
+    def top(self):
+        vecs = list(list(zip(*i)) for i in zip(*self.board))
+        vecs = [[self.reduce(v) for v in r] for r in vecs]
+        return list(zip(*[list(zip(*i)) for i in vecs]))
+
+    def bottom(self):
+        vecs = list(list(zip(*i)) for i in zip(*self.board))
+        vecs = [[self.reduce(v[::-1])[::-1] for v in r] for r in vecs]
+        return list(zip(*[list(zip(*i)) for i in vecs]))
+
+    def move(self, move):
+        moves = {"^": self.up, "v": self.down, "<": self.left, ">": self.right, "+": self.top, "-": self.bottom}
+        board = moves[move]()
+        if board == self.board:
+            return
+        else:
+            self.board = board
+            self.spawn_tile()
+            return self
+
+    def won(self):
+        return any(any(any(i is not None and i >= self.goal for i in rows) for rows in plane) for plane in self.board)
+
+    def is_endgame(self):
+        # Easy test: Check if any move can be made
+        states = self.up(), self.down(), self.left(), self.right(), self.top(), self.bottom()
+        return all(self.board == i for i in states) or self.won()
+
+    def __repr__(self):
+        return "\n....................\n".join("\n".join(" | ".join(str(i) for i in j) for j in self.board))
+
+class NegaBoard(Board):
+    pass
