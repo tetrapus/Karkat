f113e25c1c98f8c91db5267b2e99b0f2240b3d64
Joey Tuong <joey@tetrap.us>
2014-03-26 00:08:25 +1100

    stuff

--- plugins/games/social2048/boards.py
+++ plugins/games/social2048/boards.py
@@ -105,7 +105,7 @@ class FibBoard:
     fibs = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]
 
     def is_tile(self, x):
-        return x in fibs
+        return x in self.fibs
 
     def random_tile(self):
         return super().random_tile() / 2
