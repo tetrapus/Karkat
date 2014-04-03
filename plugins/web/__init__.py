504af548b3e4510adf11f2932053d853ce0e77d0
Joey Tuong <joey@tetrap.us>
2014-04-03 21:39:15 +1100

    Added summariser

--- plugins/web/__init__.py
+++ plugins/web/__init__.py
@@ -1,1 +1,1 @@
-__modules__ = ["wolfram", "google", "lastfm", "shorten", "suggest", "youtube", "filth", "snapchat", "images"]
+__modules__ = ["wolfram", "google", "lastfm", "shorten", "suggest", "youtube", "filth", "snapchat", "images", "preview"]
---
+++ plugins/web/preview.py
@@ -0,0 +1,30 @@
+import sys
+import yaml
+import functools
+import requests
+
+from bot.events import Callback, command
+
+
+class Summary(Callback):
+    API_URL = "http://api.smmry.com/"
+
+    def __init__(self, server, key=None):
+        self.key = key
+
+        super().__init__(server)
+
+    @command("preview", r"(.+)")
+    def preview(self, server, message, url):
+        params = {"SM_API_KEY": self.key,
+                  "SM_URL": url,
+                  "SM_LENGTH": 1}
+        summary = requests.get(self.API_URL, params=params).json()
+        
+        return "12│ " + summary["sm_api_content"]
+        
+try:
+    apikeys = yaml.safe_load(open("config/apikeys.conf"))["smmry"]
+    __initialise__ = functools.partial(Summary, keywords={"key": apikeys["key"]})
+except:
+    print("Preview requires a smmry API key. Skipping module.", sys.stderr)
