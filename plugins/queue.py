601759e7fafbd7a22ee35a8ddd8adc120025acc9
Joey Tuong <joey@tetrap.us>
2014-04-03 20:05:36 +1100

    tweaked queue interface

--- plugins/queue.py
+++ plugins/queue.py
@@ -24,6 +24,9 @@ class Queue(Callback):
             return
         for i, item in enumerate(queue):
             yield "06│ %d │ %s" % (i+1, item)
+            if i > 3 and message.prefix != "!":
+                yield "06│ %d of %d items displayed." % (i+1, len(queue))
+                return
 
     @command("queue", r"(.+)")
     def queue(self, server, message, item):
@@ -46,9 +49,34 @@ class Queue(Callback):
             self.save()
             return "06│ Popped item %s: %s" % (number, item)
 
+    @command("peek")
+    def peek(self, server, message):
+        nick = message.address.nick
+        queue = self.queues.setdefault(server.lower(nick), [])
+        if not queue:
+            return "06│ Your queue is empty. "
+        return "06│ %s" % queue[0]
+        
+    @command("next", r"(.+)")
+    def promote(self, server, message, item):
+        nick = message.address.nick
+        queue = self.queues.setdefault(server.lower(nick), [])
+        try:
+            if item.isdigit():
+                item = queue[int(item) - 1]
+            elif item in queue:
+                queue.remove(item)
+            queue.insert(0, item)
+        except IndexError:
+            return "06│ No such item. "
+        else:
+            return "06│ Promoted '%s'" % item
+            
+
+
     def save(self):
         with open(self.qfile, "w") as f:
             json.dump(self.queues, f)
-            
+
 
 __initialise__ = Queue
