f1a5c5dd46c781527ca51e2087f8f895b04a882d
Joey Tuong <joey@tetrap.us>
2014-04-06 19:30:59 +1000

    Fixed bgcolour

--- plugins/web/snapchat.py
+++ plugins/web/snapchat.py
@@ -327,7 +327,7 @@ class Snap(Callback):
             raise Exception("Failed to upload snap.")
         acc.send(media_id, user, time=time)
         
-    @command("reply", r"(-[maciulrdsbfp1-9]+)?(?:\s+(https?://\S+|:.+?:))?(?:\s+(.+))?")
+    @command("reply", r"(?:(-[maciulrdsbfp1-9]+)\s+)?(?:(https?://\S+|:.+?:)\s+)?(.+)?")
     def snapreply(self, server, message, flags, background, text):
         user = self.settings[server.lower(message.context)]["history"][-1]["sender"]
         yield from self.snap.funct(self, server, message, flags, user, background, text)
