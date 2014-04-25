ea1f88c35be8a493cb8311d87733de0885661920
Joey Tuong <joey@tetrap.us>
2014-04-25 18:28:36 +1000

    Changed interface, unified search

--- plugins/queue.py
+++ plugins/queue.py
@@ -21,42 +21,45 @@ class Queue(Callback):
 
     def get_queue(self, nick, query=None):
         queue = self.queues.setdefault(nick, [])
-        query = set(shlex.split(query.lower()))
 
-        q = list(enumerate(queue))
+        q = [(i[0] + 1, i[1]) for i in enumerate(queue)]
 
-        if query is not None:
-            exclude = {i for i in query if i.startswith("-")}
-            include = query - exclude
-            q = [i for i in q if all(k.lstrip("#") in [j.lstrip("#") for j in i[1].lower().split()] for k in include)]
-            q = [i for i in q if not any(k[1:].lstrip("#") in [j.lstrip("#") for j in i[1].lower().split()] for k in exclude)]
+        if query is None:
+            return q
+        elif re.match(r"\d+(-\d*)?", query):
+            index = query.split('-')
+            if len(index) == 1:
+                return q[int(index[0])]
+            elif not index[1]:
+                return q[int(index[0]):]
+            else:
+                return q[int(index[0]):int(index[1])]
+        else:
+            exact = [i for i in q if i[1].lower() == query.lower]
+            if exact:
+                return exact
+            else:
+                query = set(shlex.split(query.lower()))
+                hidden = {"hidden"} - query
+                exclude = {i for i in query if i.startswith("-")} | {'-' + i for i in hidden}
+                include = query - exclude
+                q = [i for i in q if all(k.lstrip("#") in [j.lstrip("#") for j in i[1].lower().split()] for k in include)]
+                q = [i for i in q if not any(k[1:].lstrip("#") in [j.lstrip("#") for j in i[1].lower().split()] for k in exclude)]
 
-        return q
+                return q
+
+    def display(self, line):
+        return "06│ %d │ %s" % (line[0], re.sub(r"(#\S+)", r"15\1", line[1]))
+
+    def displayAll(self, lines, max=25):
+        for count, i in enumerate(lines):
+            if max <= count and max != len(lines):
+                yield "06│ %d of %d items displayed." % (count, len(lines))
+                return
+            yield self.display(i)
 
     @command("list", r"(.*)")
-    def list(self, server, message, query):
-        nick = server.lower(message.address.nick)
-        queue = self.queues.setdefault(nick, [])
-        if not queue:
-            yield "06│ Your queue is empty. "
-            return
-
-        q = self.get_queue(nick, query)
-
-        if not q:
-            yield "06│ No matching items."
-            return
-
-        count = 0
-        for i, item in q:
-            if count > 3 and message.prefix != "!":
-                yield "06│ %d of %d items displayed." % (count, len(q))
-                return
-            yield "06│ %d │ %s" % (i+1, re.sub("(#\S+)", r"15\1", item))
-            count += 1
-
-    @command("choose", r"([^\d,]*)")
-    def choose(self, server, msg, query):
+    def list(self, server, msg, query):
         nick = server.lower(msg.address.nick)
         queue = self.queues.setdefault(nick, [])
         if not queue:
@@ -68,88 +71,138 @@ class Queue(Callback):
         if not q:
             yield "06│ No matching items."
             return
-        yield "06│ " + random.choice(q)[1]
+
+        yield from self.displayAll(q, 25 if msg.prefix == '!' else 5)
 
 
+    @command("choose", r"([^\d,]*)")
+    def choose(self, server, msg, query):
+        nick = server.lower(msg.address.nick)
+        queue = self.queues.setdefault(nick, [])
+        if not queue:
+            return "06│ Your queue is empty. "
 
-    @command("queue", r"(.+)")
+        q = self.get_queue(nick, query)
+
+        if not q:
+            return "06│ No matching items."
+
+        return self.display(random.choice(q))
+
+    @command("queue todo", r"(.+)")
     def queue(self, server, message, item):
         nick = message.address.nick
         queue = self.queues.setdefault(server.lower(nick), [])
         queue.append(item)
         self.save()
-        return "06│ Added item %d: %s" % (len(queue), re.sub("(#\S+)", r"15\1", item))
+        return self.display((len(queue), item))
 
-    @command("pop", r"(\d*)")
-    def pop(self, server, message, number):
-        nick = message.address.nick
-        queue = self.queues.setdefault(server.lower(nick), [])
-        try:
-            if not number: number = 1
-            item = queue.pop(int(number) - 1)
-        except IndexError:
-            return "06│ No such item."
-        else:
-            self.save()
-            return "06│ Popped item %s: %s" % (number, re.sub("(#\S+)", r"15\1", item))
+    @command("pop", r"(.+)")
+    def pop(self, server, msg, query):
+        nick = server.lower(msg.address.nick)
+        queue = self.queues.setdefault(nick, [])
+        if not queue:
+            yield "06│ Your queue is empty. "
+            return
 
-    @command("peek")
-    def peek(self, server, message):
-        nick = message.address.nick
-        queue = self.queues.setdefault(server.lower(nick), [])
+        q = self.get_queue(nick, query)
+
+        if not q:
+            yield "06│ No matching items."
+            return
+
+        for i in q:
+            queue.pop(i[0]-1)
+
+        yield from self.displayAll(q, 25 if msg.prefix == '!' else 5)
+
+        self.save()
+
+    @command("peek", r"(.*)")
+    def peek(self, server, message, query):
+        nick = server.lower(message.address.nick)
+        queue = self.queues.setdefault(nick, [])
         if not queue:
             return "06│ Your queue is empty. "
-        return "06│ %s" % re.sub("(#\S+)", r"15\1", queue[0])
+
+        q = self.get_queue(nick, query)
+
+        if not q:
+            return "06│ No matching items."
+
+        return "06│ %s" % self.display(q[0][1])
         
-    @command("next", r"(.+)")
-    def promote(self, server, message, item):
-        nick = message.address.nick
-        queue = self.queues.setdefault(server.lower(nick), [])
-        try:
-            if item.isdigit():
-                item = queue[int(item) - 1]
-            if item in queue:
-                queue.remove(item)
+    @command("next promote", r"(.+)")
+    def promote(self, server, msg, query):
+        nick = server.lower(msg.address.nick)
+        queue = self.queues.setdefault(nick, [])
+        if not queue:
+            yield "06│ Your queue is empty. "
+            return
+        q = self.get_queue(nick, query)
+
+        if not q:
+            yield "06│ No matching items."
+            return
+        for i, item in q[::-1]:
+            queue.pop(i-1)
             queue.insert(0, item)
-        except IndexError:
-            return "06│ No such item. "
-        else:
-            return "06│ Promoted '%s'" % (re.sub("(#\S+)", r"15\1", item))
+
+        yield from self.displayAll([(i+1, item[1]) for i, item in enumerate(q)], 25 if msg.prefix == '!' else 5)
+
+        self.save()
             
     @command("tag", r"(#\S+(?:\s+#\S+)*)\s+(.+)")
-    def tag(self, server, message, tag, item):
-        nick = message.address.nick
-        queue = self.queues.setdefault(server.lower(nick), [])
-        try:
-            if all(i.isdigit() for i in item.split()):
-                items = [int(i) - 1 for i in item.split()]
-            else:
-                items = [queue.index(item)]
-        except:
-            return "06│ No such item."
-        for i in items:
-            queue[i] = queue[i] + " " + tag
+    def tag(self, server, msg, tag, query):
+        nick = server.lower(msg.address.nick)
+        queue = self.queues.setdefault(nick, [])
+        if not queue:
+            yield "06│ Your queue is empty. "
+            return
+        q = self.get_queue(nick, query)
+
+        if not q:
+            yield "06│ No matching items."
+            return
+
+        for i, item in q:
+            tags = [i for i in tag.split() if i.lower() not in item.lower()]
+            queue[i-1] = item + ' ' + ' '.join(tags)
+
+        yield from self.displayAll([(i, queue[i-1]) for i in q], 25 if msg.prefix == '!' else 5)
+
         self.save()
-        return "06│ Added tags."
 
     @command("untag", r"(#\S+(?:\s+#\S+)*)(?:\s+(.+))?")
-    def untag(self, server, message, tags, item):
-        nick = message.address.nick
-        queue = self.queues.setdefault(server.lower(nick), [])
+    def untag(self, server, msg, tags, query):
+        nick = server.lower(msg.address.nick)
+        queue = self.queues.setdefault(nick, [])
         tags = tags.split()
-        try:
-            if not item:
-                items = range(len(queue))
-            elif all(i.isdigit() for i in item.split()):
-                items = [int(i) - 1 for i in item.split()]
-            else:
-                items = [queue.index(item)]
-        except:
-            return "06│ No such item."
-        for i in items:
-            queue[i] = re.sub("( ?#(%s))" % ("|".join(re.escape(x) for x in tags)), "", queue[i], re.IGNORECASE)
+        if not queue:
+            yield "06│ Your queue is empty. "
+            return
+        q = self.get_queue(nick, query)
+
+        if not q:
+            yield "06│ No matching items."
+            return
+
+        for i, item in q:
+            queue[i-1] = re.sub("( ?#(%s))" % ("|".join(re.escape(x) for x in tags)), "", queue[i], re.IGNORECASE)
+
+        yield from self.displayAll([(i, queue[i-1]) for i in q], 25 if msg.prefix == '!' else 5)
+
         self.save()
-        return "06│ Added tags."
+
+    # TODO: Alter hidden tags
+    @command("hide", r"(.+)")
+    def hide(self, server, msg, query):
+        yield from self.tag.funct(self, server, msg, "#hidden", query)
+
+    # TODO: Alter hidden tags
+    @command("unhide", r"(.+)")
+    def unhide(self, server, msg, query):
+        yield from self.untag.funct(self, server, msg, "#hidden", query)        
 
     def save(self):
         with open(self.qfile, "w") as f:
