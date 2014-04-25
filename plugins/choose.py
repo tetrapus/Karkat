1843da08486144a0e4de5eab8c3ab4a6e543d51f
Joey Tuong <joey@tetrap.us>
2014-04-25 23:40:04 +1000

    Fixed bugs

--- plugins/choose.py
+++ plugins/choose.py
@@ -6,6 +6,7 @@ from bot.events import command, Callback
 @Callback.threadsafe
 @command("choose", "(.+)", prefixes=("", "."))
 def choose(server, msg, query):
+    if "," not in query: return
     return "\x0309│\x03 " + random.choice(re.split(r",|\bor\b", query)).strip()
 
 __callbacks__ = {"privmsg": [choose]}
