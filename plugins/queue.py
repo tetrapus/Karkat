c12d74b6b13194a719ce09a5eb965bd3a3c7712b
Joey Tuong <joey@tetrap.us>
2014-09-15 10:12:32 +1000

    fixed import limits

--- plugins/queue.py
+++ plugins/queue.py
@@ -4,9 +4,80 @@ import re
 import random
 import math
 import requests
+import operator
 
 from bot.events import Callback, command
 from util.text import strikethrough, smallcaps
+
+def numeric_priority(item):
+    m = re.match(r"^\((-?\d+(?:\.\d+)?)\)\s+.*$", item)
+    if not m:
+        return 0
+    else:
+        return float(m.group(1))
+
+def alpha_priority(letters):
+    return sum((ord(a) - ord("A") + 1) * 26 ** i for i, a in enumerate(letters[::-1]))
+
+def priority_sorted(queue):
+    """ 
+    An item with a priority has the form ^\(([A-Z]+|\d+(\.\d+))\)\S+(.+)$
+    where \1 is the priority and \2 is the rest of the todo list.
+
+    Alphabetic priorities are interpreted in shortlex order where the lowest
+    value corresponds to the highest priority, whereas integer priorities correspond
+    to base 10 integers where higher values correspond to higher priorities.
+
+    An alphabetic priority is converted to a numeric priority such that the highest
+    alphabetic priority corresponds with the highest numeric priority or higher, and
+    all alphabetic priorities are greater than 0. 
+
+    Items without priorities are considered to have a numeric priority of 0.
+    """
+    # Pull alphabetic priorities out of the list
+    alphabetic = []
+    numeric = []
+    for i in queue:
+        m = re.match(r"^\(([A-Z]+)\)\s+.*$", i)
+        if m:
+            alphabetic.append((alpha_priority(m.group(1)), i))
+        else:
+            numeric.append((numeric_priority(i), i))
+    alpha_priorities = {}
+    if alphabetic:
+        # Find the largest integer priority
+        largest = math.ceil(max(i[0] for i in numeric)) if numeric else 1
+        # Find lowest alphabetic priority
+        alpha_max = max(i[0] for i in alphabetic)
+        align_to = max(alpha_max, largest)
+        # Align so that alpha priority 1 == largest
+        alpha_priorities = {i: align_to-p + 1 for p, i in alphabetic}
+
+    queue = [(alphabetic_priorities[i] if i in alphabetic else numeric_priority(i), i) for i in queue]
+
+    return sorted(queue, key=operator.itemgetter(0))
+
+def priority_sort(queue):
+    # Pull alphabetic priorities out of the list
+    alphabetic = []
+    numeric = []
+    for i in queue:
+        m = re.match(r"^\(([A-Z]+)\)\s+.*$", i)
+        if m:
+            alphabetic.append((alpha_priority(m.group(1)), i))
+        else:
+            numeric.append((numeric_priority(i), i))
+    alpha_priorities = {}
+    if alphabetic:
+        # Find the largest integer priority
+        largest = math.ceil(max(i[0] for i in numeric)) if numeric else 1
+        # Find lowest alphabetic priority
+        alpha_max = max(i[0] for i in alphabetic)
+        align_to = max(alpha_max, largest)
+        # Align so that alpha priority 1 == largest
+        alpha_priorities = {i: align_to-p + 1 for p, i in alphabetic}
+
+    queue.sort(key=lambda i: alphabetic_priorities[i] if i in alphabetic else numeric_priority(i))
 
 
 class Queue(Callback):
@@ -17,10 +88,12 @@ class Queue(Callback):
         try:
             with open(self.qfile) as f:
                 self.queues = json.load(f)
+                for i in self.queues:
+                    priority_sort(self.queues[i])
         except:
             self.queues = {}
         super().__init__(server)
-        
+
 
     def find(self, queue, query=None):
         q = [(i[0] + 1, i[1]) for i in enumerate(queue)]
@@ -111,8 +184,9 @@ class Queue(Callback):
         nick = message.address.nick
         queue = self.queues.setdefault(server.lower(nick), [])
         queue.append(item)
+        priority_sort(queue)
         self.save()
-        return self.display(len(queue), item)
+        return self.display(queue.index(item) + 1, item)
 
     @command("edit replace", r"(\d+)\s+(.+)")
     def edit(self, server, message, index, item):
@@ -123,16 +197,18 @@ class Queue(Callback):
             queue.append("")
             index = len(queue)
         queue[index - 1] = item
+        priority_sort(queue)
         self.save()
-        return self.display(index, item)
+        return self.display(queue.index(item) + 1, item)
 
     @command("push prepend", r"(.+)")
     def push(self, server, message, item):
         nick = message.address.nick
         queue = self.queues.setdefault(server.lower(nick), [])
         queue.insert(0, item)
+        priority_sort(queue)
         self.save()
-        return self.display(1, item)
+        return self.display(queue.index(item) + 1, item)
 
     @command("pop", r"(.*)")
     def pop(self, server, msg, query):
@@ -156,7 +232,7 @@ class Queue(Callback):
             queue.pop(i[0]-1)
 
         yield from self.displayAll([('✓' if len(q) == 1 else i[0], i[1]) for i in q], 25 if msg.prefix == '!' else 5, strike=True)
-
+        priority_sort(queue)
         self.save()
 
     @command("peek", r"(.*)")
@@ -190,10 +266,9 @@ class Queue(Callback):
             queue.pop(i-1)
         for i, item in q:
             queue.insert(0, item)
-
-        q.reverse()
-
-        yield from self.displayAll([(i+1, item[1]) for i, item in enumerate(q)], 25 if msg.prefix == '!' else 5)
+        priority_sort(queue)
+        q = sorted([(queue.find(i[1]) + 1, i[1]) for i in q])
+        yield from self.displayAll(q, 25 if msg.prefix == '!' else 5)
 
         self.save()
             
@@ -208,8 +283,6 @@ class Queue(Callback):
             queue.append(query)
             q = [(len(queue), query)]
 
-        updated = []
-
         q.reverse()
 
         for i, item in q:
@@ -217,7 +290,9 @@ class Queue(Callback):
         q.reverse()
         for i, item in q:
             queue.append(item)
-            updated.append((len(queue), item))
+
+        priority_sort(queue)
+        updated = sorted([(queue.find(i[1]) + 1, i[1]) for i in q])
 
         yield from self.displayAll(updated, 25 if msg.prefix == '!' else 5)
 
@@ -247,8 +322,9 @@ class Queue(Callback):
             queue.pop(i-1)
             queue.insert(index, item)
         q.reverse()
-        for i, item in enumerate(q):
-            updated.append((index + i+1, item[1]))
+        priority_sort(queue)
+        for i, item in q:
+            updated.append((queue.find(item) + 1, item))
 
         yield from self.displayAll(updated, 25 if msg.prefix == '!' else 5)
 
@@ -272,7 +348,7 @@ class Queue(Callback):
             queue[i-1] = item + ' ' + ' '.join(tags)
 
         yield from self.displayAll([(i[0], queue[i[0]-1]) for i in q], 25 if msg.prefix == '!' else 5)
-
+        # TODO: Priority sort (only technically necessary)
         self.save()
 
     @command("untag", r"((?:#|\+|@)\S+(?:\s+(?:#|\+|@)\S+)*)(?:\s+(.+))?")
@@ -298,7 +374,7 @@ class Queue(Callback):
                 tagged.append((i, fixed))
 
         yield from self.displayAll(tagged, 25 if msg.prefix == '!' else 5)
-
+        # TODO: Priority sort (only technically necessary)
         self.save()
 
     @command("score", r"((?:\d+/)?\d+|[+-]\d+)\s+(.+)")
@@ -323,7 +399,7 @@ class Queue(Callback):
             # TODO: relative scoring and velocity
 
         yield from self.displayAll([(i[0], queue[i[0]-1]) for i in q], 25 if msg.prefix == '!' else 5)
-
+        # TODO: Priority sort (only technically necessary)
         self.save()
 
     # TODO: Alter hidden tags
@@ -336,11 +412,21 @@ class Queue(Callback):
     def unhide(self, server, msg, query):
         yield from self.untag.funct(self, server, msg, "#hidden", query)        
 
-    @command
-    def qexport(self, server, msg):
+    @command("qexport", r"(.*)")
+    def qexport(self, server, msg, query):
         nick = server.lower(msg.address.nick)
         queue = self.queues.setdefault(nick, [])
-        data = "\n".join(queue)
+        if not queue:
+            yield "06│ Your queue is empty. "
+            return
+
+        q = self.find(queue, query)
+
+        if not q:
+            yield "06│ No matching items."
+            return
+
+        data = "\n".join(q)
         postres = requests.post("https://api.github.com/gists", data=json.dumps({"files": {"%s-todo.txt" % (msg.address.nick): {"content": data}}}))
         response = postres.json()
         return "06│ Exported to %s." % response["html_url"]
@@ -353,7 +439,7 @@ class Queue(Callback):
         data = requests.get(url).text.split("\n")
         if merge:
             data = [i for i in data if i not in queue]
-        if len(data) > 500:
+        if sum([len(i) for i in data]) > 262144:
             yield "06│ Queue too large. Upgrade to Karkat Premium to raise your storage limit."
             return
         queue.extend(data)
