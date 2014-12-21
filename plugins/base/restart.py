177caf2005821a6035983b97a13b6153f5b99d39
Joey Tuong <joey@tetrap.us>
2014-12-21 23:01:42 +1100

    Add quit command

--- plugins/base/restart.py
+++ plugins/base/restart.py
@@ -13,4 +13,13 @@ def set_restart(server, message):
     else:
         server.printer.raw_message("QUIT :Restarting: " + message.arg)
 
-__callbacks__ = {"privmsg": [set_restart]}
+@command("quit", public=":", private="", admin=True)
+def shut_down(server, message):
+    """ Disconnect from the server, ensuring that the restart flag is unset. """
+    server.restart = False
+    if message.arg is None:
+        server.printer.raw_message("QUIT :Shutting down")
+    else:
+        server.printer.raw_message("QUIT :Shutting down: " + message.arg)
+
+__callbacks__ = {"privmsg": [set_restart, shut_down]}
