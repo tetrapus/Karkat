d435de91a3bb0c8f076ad9773ce088164a99c29a
Joey Tuong <joey@tetrap.us>
2015-07-07 17:50:22 +1000

    Style tweaks

--- plugins/web/pushbullet.py
+++ plugins/web/pushbullet.py
@@ -12,7 +12,19 @@ from util.files import Config
 from util.services import url
 from util.images import image_search
 
-HEADERS = """GET /websocket/%(key)s HTTP/1.1\r\nHost: stream.pushbullet.com\r\nUser-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\nSec-WebSocket-Version: 13\r\nSec-WebSocket-Key: VwQcElXiDe4ZsgAPdAur0g==\r\nConnection: keep-alive, Upgrade\r\nPragma: no-cache\r\nCache-Control: no-cache\r\nUpgrade: websocket\r\n\r\n"""
+HEADERS = ("GET /websocket/%(key)s HTTP/1.1\r\n"
+          "Host: stream.pushbullet.com\r\n"
+          "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0\r\n"
+          "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
+          "Accept-Language: en-US,en;q=0.5\r\n"
+          "Accept-Encoding: gzip, deflate\r\n"
+          "Sec-WebSocket-Version: 13\r\n"
+          "Sec-WebSocket-Key: VwQcElXiDe4ZsgAPdAur0g==\r\n"
+          "Connection: keep-alive, Upgrade\r\n"
+          "Pragma: no-cache\r\n"
+          "Cache-Control: no-cache\r\n"
+          "Upgrade: websocket\r\n"
+          "\r\n")
 
 class WebSocket(object):
     def __init__(self, sock, buff=b''):
