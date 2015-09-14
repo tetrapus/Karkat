690a3e5719d16dc59a39917e158de5bf7909df57
Joey Tuong <joey@tetrap.us>
2015-09-14 19:42:17 +1000

    update output format, fix channel titles

--- plugins/web/youtube.py
+++ plugins/web/youtube.py
@@ -1,10 +1,11 @@
 from util.services.youtube import youtube as yt
+from util import parallelise
 
 from util.irc import Callback, command
 
-templates = {"@": "04│ ▶ │ %(title)s\n04│ ▶ │ 15by %(channel)s 12↗ http://youtu.be/%(url)s",
+templates = {"@": "04│ %(title)s 12↗ http://youtu.be/%(url)s\n04│ by %(channel)s · %(views)s views · %(likebar)s",
              ".": "04│ %(title)s · by %(channel)s 12↗ http://youtu.be/%(url)s",
-             "!": "04│ ▶ │ %(title)s\n04│ ▶ │ 15by %(channel)s 12↗ http://youtu.be/%(url)s"}
+             "!": "04│ %(title)s 12↗ http://youtu.be/%(url)s\n04│ by %(channel)s · %(views)s views · %(likebar)s"}
 
 lines = {"@": 1,
          ".": 1,
@@ -12,11 +13,24 @@ lines = {"@": 1,
 
 @Callback.background
 def refresh_tokens(server, line):
-    if yt.tokensExpired():
-        yt.refresh_tokens()
+    with yt.keylock:
+        if yt.tokensExpired():
+            yt.refresh_tokens()
+
+
+def likebar(likes, dislikes, minwidth=16):
+    likestr = "👍 {:,}".format(likes)
+    dislikestr = "👎 {:,}".format(dislikes)
+    spaces = max(minwidth - len(likestr) - len(dislikestr), 0) + 1
+    bar = list(likestr + (" " * spaces) + dislikestr)
+    width = len(bar)
+    if likes and dislikes:
+        ratio = int(likes * width / (likes + dislikes))
+        bar.insert(ratio, "\x0315")
+    return "\x0312\x1f" + "".join(bar) + "\x1f"
 
 @Callback.threadsafe
-@command(["youtube", "yt"], "(-\d\s+)?(.+)", public=".@", private="!",
+@command(["youtube", "yt"], r"(-\d\s+)?(.+)", public=".@", private="!",
             usage="04│ ▶ │ Usage: [.@]youtube [-NUM_RESULTS] <query>",
             error="04│ ▶ │ Failed to get search results.")
 def youtube(server, message, nresults, query):
@@ -29,8 +43,17 @@ def youtube(server, message, nresults, query):
 
     for i in results:
         data = {"title": i["snippet"]["title"],
-                "channel": i["snippet"]["channelTitle"],
                 "url": i["id"]["videoId"]}
+
+        if message.prefix != ".":
+            channelinfo, stats = parallelise([lambda: yt.get_channel_info(i["snippet"]["channelId"]),
+                                              lambda: yt.stats(i["id"]["videoId"])])
+            data["likebar"] = likebar(int(stats["likeCount"]), int(stats["dislikeCount"]))
+            data["views"] = "{:,}".format(stats["viewCount"])
+            data["channel"] = channelinfo["title"]
+        else:
+            channelinfo = yt.get_channel_info(i["snippet"]["channelId"])
+            data["channel"] = channelinfo["title"]
         yield templates[message.prefix] % data
 
 __callbacks__ = {"privmsg": [youtube],
