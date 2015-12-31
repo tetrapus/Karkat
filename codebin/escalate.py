679cc165c1442439e878f9d0da17bddd418345e2
Joey Tuong <joey@tetrap.us>
2016-01-01 05:08:33 +1100

    [cleanup] Move codebin to new branch

--- codebin/escalate.py
+++
@@ -1,9 +0,0 @@
-from irc import Callback
-
-def __initialise__(name, bot, stream):
-	cb = Callback()
-	cb.initialise(name, bot, stream)
-	@cb.command("escalate", "(.+)", help="12Escalate05⎟ Sets temporary admin permissions on your hostmask.\n"
-										 "12        05⎟ Usage: [!@]escalate <password>")
-	def escalate(message, password):
-		if password == 
