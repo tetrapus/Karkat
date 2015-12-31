0c6b1f9187939d776c5142a477bfcaee105685a9
Joey Tuong <joey@tetrap.us>
2016-01-01 05:25:02 +1100

    Deprecate snapchat (see snapchat-deprecated branch for dev)

--- plugins/web/__init__.py
+++ plugins/web/__init__.py
@@ -1,1 +1,19 @@
-__modules__ = ["wolfram", "google", "lastfm", "shorten", "suggest", "youtube", "filth", "images", "preview", "urban", "yo", "fiftyfifty", "weather", "pushbullet", "howold"]
+""" Web-based plugins. """
+
+__modules__ = [
+    "wolfram",
+    "google",
+    "lastfm",
+    "shorten",
+    "suggest",
+    "youtube",
+    "filth",
+    "images",
+    "preview",
+    "urban",
+    "yo",
+    "fiftyfifty",
+    "weather",
+    "pushbullet",
+    "howold",
+]
