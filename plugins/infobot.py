db2f0efc7463fefaa3e49ee968084e2bba93bf03
Joey Tuong <joey@tetrap.us>
2016-05-14 22:14:30 +1000

    Clean up info module
    - Remove ! filters to use standard command prefixes
    - Some lint fixes

--- plugins/infobot.py
+++ plugins/infobot.py
@@ -1,16 +1,26 @@
-from bot.events import command, Callback
+"""
+Provides .info and related commands. Allows users to set information about themselves that may be
+retrieved by other users.
+"""
 import json
 
-infofile = "info.json"
-undofile = "undo.json"
-protectionfile = "users.json"
+from bot.events import command, Callback
 
-def check_whois(gen, server, msg, user):
+
+INFO_PATH = "info.json"
+UNDO_PATH = "undo.json"
+PROTECTION_PATH = "users.json"
+
+
+def check_whois(gen, server, msg, _):
+    ''' Generate and catch a whois event for a given user '''
     server.whois_waiting[server.lower(msg.address.nick)] = (gen, msg)
     server.printer.raw_message("WHOIS :%s" % msg.address.nick)
 
+
 @Callback.inline
 def finish_whois(server, line):
+    ''' Resume execution of a generator waiting on a WHOIS event '''
     words = line.split()
     nick = server.lower(words[3])
     if nick in server.whois_waiting:
@@ -21,30 +31,36 @@ def finish_whois(server, line):
             for i in gen:
                 out += i
 
+
 def isprotected(server, user):
+    ''' Check if the given user has opted to protect their info. '''
     try:
-        protected = json.load(open(server.get_config_dir(protectionfile)))
+        protected = json.load(open(server.get_config_dir(PROTECTION_PATH)))
     except:
         protected = {}
     return server.lower(user) in protected and protected[server.lower(user)]
 
+
 def set_protected(server, user):
     """ Add protection if not defined """
     try:
-        protected = json.load(open(server.get_config_dir(protectionfile)))
+        protected = json.load(open(server.get_config_dir(PROTECTION_PATH)))
     except:
         protected = {}
     if server.lower(user) not in protected:
         protected[server.lower(user)] = True
-    json.dump(protected, open(server.get_config_dir(protectionfile), "w"))
+    json.dump(protected, open(server.get_config_dir(PROTECTION_PATH), "w"))
+
 
 @command("protect", r"(on|off|none)?")
 def toggle_protection(server, msg, state):
+    ''' Toggle a user's protection state '''
     user = msg.address.nick
     luser = server.lower(user)
+    # NOTE: I have no idea what the following code does?
     registered = server.registered.get(luser, False)
     try:
-        protected = json.load(open(server.get_config_dir(protectionfile)))
+        protected = json.load(open(server.get_config_dir(PROTECTION_PATH)))
     except:
         protected = {}
     if state == "on":
@@ -55,35 +71,45 @@ def toggle_protection(server, msg, state):
         del protected[luser]
     else:
         protected[luser] = not protected.get(luser, False)
-    json.dump(protected, open(server.get_config_dir(protectionfile), "w"))
-    return "│ Account protection for %s is %s" %(user, "CLEARED" if luser not in protected else ["OFF", "ON"][protected[luser]])
-    
+    json.dump(protected, open(server.get_config_dir(PROTECTION_PATH), "w"))
+    return "│ Account protection for {user} is {action}".format(
+        user=user,
+        action="CLEARED" if luser not in protected else ["OFF", "ON"][protected[luser]],
+    )
 
-@command("info", r"(.*)", prefixes=("", "."))
+
+@command("info", r"(.*)")
 def getinfo(server, msg, user):
+    ''' Retrieve a user's info. '''
     try:
-        data = json.load(open(server.get_config_dir(infofile)))
+        data = json.load(open(server.get_config_dir(INFO_PATH)))
     except:
         data = {}
     user = user or msg.address.nick
     luser = server.lower(user)
     protected = isprotected(server, user)
     if luser not in data:
-        return "I don't have info about %s. %s can use \x0312!setinfo \x1fblurb\x0f to add their info, or try !info %s if another bot provides this service." % (user, user, user)
+        return (
+            "I don't have info about {user}. "
+            "{user} can use \x0312!setinfo \x1fblurb\x0f to add their info, "
+            "or try !info {user} if another bot provides this service.".format(user=user)
+        )
     elif protected:
-        return "\x0312%s\x03: %s" % (user, data[luser])
+        return "\x0312{user}\x03: {info}".format(user=user, info=data[luser])
     else:
-        return "%s: %s" % (user, data[luser])
+        return "{user}: {info}".format(user=user, info=data[luser])
 
-@command("setinfo", r"(.*)", prefixes=("!", "."))
+
+@command("setinfo", r"(.*)")
 def setinfo(server, msg, info):
+    ''' Set your .info text. '''
     yield check_whois
     try:
-        data = json.load(open(server.get_config_dir(infofile)))
+        data = json.load(open(server.get_config_dir(INFO_PATH)))
     except:
         data = {}
     try:
-        undo = json.load(open(server.get_config_dir(undofile)))
+        undo = json.load(open(server.get_config_dir(UNDO_PATH)))
     except:
         undo = {}
     user = msg.address.nick
@@ -92,8 +118,10 @@ def setinfo(server, msg, info):
     registered = server.registered.get(luser, False)
 
     if protected and not registered:
-        if msg.prefix != "!":
-            yield "│ This nickname is protected by the owner. Please identify with NickServ to update your info."
+        yield (
+            "│ This nickname is protected by the owner. "
+            "Please identify with NickServ to update your info."
+        )
         return
 
     if luser in data:
@@ -101,28 +129,28 @@ def setinfo(server, msg, info):
 
     if not info:
         del data[luser]
-        if msg.prefix != "!":
-            yield "│ Your information has been deleted."
+        yield "│ Your information has been deleted."
     else:
         data[luser] = info
-        if msg.prefix != "!":
-            yield "│ Your information has been updated."
+        yield "│ Your information has been updated."
 
     if registered:
         set_protected(server, user)
 
-    json.dump(data, open(server.get_config_dir(infofile), "w"))
-    json.dump(undo, open(server.get_config_dir(undofile), "w"))
+    json.dump(data, open(server.get_config_dir(INFO_PATH), "w"))
+    json.dump(undo, open(server.get_config_dir(UNDO_PATH), "w"))
 
-@command("undo", prefixes=("!", "."))
+
+@command("undo")
 def undoinfo(server, msg):
+    ''' Undo the last .setinfo '''
     yield lambda x, y, z: check_whois(x, y, z, "")
     try:
-        data = json.load(open(server.get_config_dir(infofile)))
+        data = json.load(open(server.get_config_dir(INFO_PATH)))
     except:
         data = {}
     try:
-        undo = json.load(open(server.get_config_dir(undofile)))
+        undo = json.load(open(server.get_config_dir(UNDO_PATH)))
     except:
         undo = {}
     user = msg.address.nick
@@ -131,8 +159,10 @@ def undoinfo(server, msg):
     registered = server.registered.get(luser, False)
 
     if protected and not registered:
-        if msg.prefix != "!":
-            yield "│ This nickname is protected by the owner. Please identify with NickServ to update your info."
+        yield (
+            "│ This nickname is protected by the owner. "
+            "Please identify with NickServ to update your info."
+        )
         return
 
     if luser not in undo or luser not in data:
@@ -141,19 +171,19 @@ def undoinfo(server, msg):
     undo[luser], data[luser] = data[luser], undo[luser]
 
     if not data[luser]:
-        if msg.prefix != "!":
-            yield "│ Your information has been deleted."
+        yield "│ Your information has been deleted."
     else:
-        if msg.prefix != "!":
-            yield "│ Your information has been reverted."
+        yield "│ Your information has been reverted."
 
     if registered:
         set_protected(server, user)
 
-    json.dump(data, open(server.get_config_dir(infofile), "w"))
-    json.dump(undo, open(server.get_config_dir(undofile), "w"))
+    json.dump(data, open(server.get_config_dir(INFO_PATH), "w"))
+    json.dump(undo, open(server.get_config_dir(UNDO_PATH), "w"))
+
 
 __callbacks__ = {"privmsg": [getinfo, setinfo, toggle_protection, undoinfo], "318": [finish_whois]}
+
 
 def __initialise__(server):
     server.whois_waiting = {}
